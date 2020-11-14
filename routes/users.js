792489171cb0becce02520f880ffd195dab8d0dd
basura-persistent <57583373+basura-persistent@users.noreply.github.com>
2020-11-14 13:37:28 -0800

    WORKS

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var db=require('../database');
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// this script to fetch data from MySQL databse table
+router.get('/user-list', function(req, res, next) {
+  var sql='SELECT * FROM BK_Student';
+  db.query(sql, function (err, data, fields) {
+  if (err) throw err;
+  res.render('user-list', { title: 'User List', userData: data});
+});
+});
+
+module.exports = router;
